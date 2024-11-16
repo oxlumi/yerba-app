fd510e974e318af75d09ddf56bdb7c39dd86b6c0
d4rm5 <dantemartinez06@gmail.com>
2024-11-17 03:17:08 +0700

    Supersim localhost

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,27 @@
-import { connectorsForWallets } from '@rainbow-me/rainbowkit';
+import { connectorsForWallets, Chain} from '@rainbow-me/rainbowkit';
 import { rainbowWallet, walletConnectWallet, injectedWallet } from '@rainbow-me/rainbowkit/wallets';
 import { createConfig, http, cookieStorage, createStorage } from 'wagmi';
-import { localhost, sepolia } from 'wagmi/chains';
 import { getConfig } from '~/config';
 
 const { PROJECT_ID } = getConfig().env;
+
+const supersim = {
+  id: 901,
+  name: 'Supersim',
+  iconUrl: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+  iconBackground: '#fff',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['http://127.0.0.1:9545'] },
+  },
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 11_907_934,
+    },
+  },
+} as const satisfies Chain;
+
 
 const getWallets = () => {
   if (PROJECT_ID) {
@@ -22,20 +39,19 @@ const connectors = connectorsForWallets(
     },
   ],
   {
-    appName: 'Yerba Coffee',
+    appName: 'Web3 React boilerplate',
     projectId: PROJECT_ID,
   },
 );
 
 export const config = createConfig({
-  chains: [localhost, sepolia],
+  chains: [supersim],
   ssr: true,
   storage: createStorage({
     storage: cookieStorage,
   }),
   transports: {
-    [localhost.id]: http(),
-    [sepolia.id]: http(),
+    [supersim.id]: http(),
   },
   batch: { multicall: true },
   connectors,
